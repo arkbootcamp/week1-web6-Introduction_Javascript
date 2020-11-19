e754b33afad79a0cb482dc965798069f79d6c38f
Bagusth15 <bagustri15@gmail.com>
2020-11-19 16:41:56 +0700

    update function

--- Part 2/function.js
+++ Part 2/function.js
@@ -1,14 +1,17 @@
+// DECLARATION FUNCTION = memiliki sifat hoisting yang dimana untuk fungsinya bisa diletakkan dimana saja
 // test();
 // function test() {
 //   return console.log("Berhasil testing !");
 // }
 // test();
 
+// EXPRESSION FUNCTION = fungsinya disimpan ke dalam variabel jadi pemanggilannya harus dibawah fungsi itu berasa
 // const test = () => {
 //   return console.log("Berhasil testing !");
 // };
 // test();
 
+// =================================================================================================
 const sum = (param) => {
   // console.log(param1); // 1
   // console.log(param2); // 2
@@ -24,6 +27,7 @@ function loop() {
   let a = 0;
   for (let i = 0; i <= 5; i++) {
     a += i;
+    // return tidak bisa diletakkan di dalam for karena fungsi akan membaca bahwa return sebagai akhir dari program
   }
   return a;
 }
